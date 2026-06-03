260153286c6dc06374b9574360b7d708b417735f
Lucas Ng <141851379+lucasnye@users.noreply.github.com>
2026-06-04 00:57:47 +0800

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.py
@@ -0,0 +1,16 @@
+# Top-down DP (Memoization)
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = {}
+        def dfs(r, c):
+            if r == m - 1 or c == n - 1:
+                return 1
+            
+            if (r, c) in dp:
+                return dp[(r, c)]
+            
+            dp[(r, c)] = dfs(r + 1, c) + dfs(r, c + 1)
+            
+            return dp[(r, c)]
+        
+        return dfs(0, 0)
